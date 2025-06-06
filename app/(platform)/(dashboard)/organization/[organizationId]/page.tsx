462b40f7d43d26abb9fcce2a684b4d3930a7183c
chris-rutter-api <christian@rutterapi.com>
2025-06-06 00:51:21 -0300

    feat: adding board list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react"
+
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
 import { BoardList } from "./_components/board-list";
@@ -9,7 +11,9 @@ export const OrganizationIdPage = async () => {
             <Info />
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
-                <BoardList />
+                <Suspense fallback={<BoardList.Skeleton />}>
+                    <BoardList />
+                </Suspense>
             </div>
         </div>
     )
